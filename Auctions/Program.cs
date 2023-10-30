5056e5347ef6d6f76feb2218493610a1d1061e79
Vndark5464 <nguyenhuulinh5464@gmail.com>
2023-10-31 04:15:05 +0700

    a

--- Auctions/Program.cs
+++ Auctions/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IListingsService, ListingsService>();
+builder.Services.AddScoped<IBidsService, BidsService>();
+builder.Services.AddScoped<ICommentsService, CommentsService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
