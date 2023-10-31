3ff25ec4cb24a2aafdea4cc792274eabb042d30b
Vndark5464 <nguyenhuulinh5464@gmail.com>
2023-10-31 08:47:09 +0700

    a

---
+++ Auctions/Controllers/AdminController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+public class AdminController : Controller
+{
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AdminController(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public IActionResult ManageUsers()
+    {
+        var users = _userManager.Users.ToList();
+        return View(users);
+    }
+
+    public async Task<IActionResult> EditUser(string id)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return View(user);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> EditUser(string id, IdentityUser model)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        user.Email = model.Email;
+        user.UserName = model.UserName;
+        var result = await _userManager.UpdateAsync(user);
+        if (result.Succeeded)
+        {
+            return RedirectToAction("ManageUsers");
+        }
+        return View(user);
+    }
+
+    public async Task<IActionResult> DeleteUser(string id)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return View(user);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> DeleteUserConfirmed(string id)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        await _userManager.DeleteAsync(user);
+        return RedirectToAction("ManageUsers");
+    }
+}
